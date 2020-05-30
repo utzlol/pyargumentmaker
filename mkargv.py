00ef1218a38fe664efa8c87bc7cf1a291c6e27ec
utzlol <40377777+utzlol@users.noreply.github.com>
2020-05-30 17:23:28 +0700

    Typo Fix

--- mkargv.py
+++ mkargv.py
@@ -1,6 +1,6 @@
 import sys
 def makeargument(argvoption, command):
-    if len(sys.argv) > 2:   #To prevent more than two arguments
+    if len(sys.argv) > 2:   #To prevent more than one arguments
         print("Too many arguments")
     if len(sys.argv) > 1:
         if sys.argv[1] == argvoption:
