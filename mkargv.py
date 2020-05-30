76df5c74db9a5b958262f673b1603e61a8b204b8
utzlol <40377777+utzlol@users.noreply.github.com>
2020-05-30 17:21:51 +0700

    Update mkargv.py

--- mkargv.py
+++ mkargv.py
@@ -1,10 +1,7 @@
 import sys
-def mkargv(argvoption, command):
-    print(len(sys.argv))
+def makeargument(argvoption, command):
     if len(sys.argv) > 2:   #To prevent more than two arguments
         print("Too many arguments")
     if len(sys.argv) > 1:
         if sys.argv[1] == argvoption:
             exec(command)
-            
-mkargv("yudha", "print('Hi')")
